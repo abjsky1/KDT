6d5ba688cdbdd1cc31e3256017f0a8c774d5d40c
abjsky1 <abjsky1@gmail.com>
2026-07-07 18:29:37 +0900

    practice1_JS실습2_문제1

--- day02/practice1.js
+++ day02/practice1.js
@@ -1,3 +1,33 @@
 let a1
-a1 = prompt("이름이 무엇인가요?")
+a1 = prompt("이름을 적어주세요.")
 console.log(a1)
+
+let a2
+a2 = prompt("원하는 음료를 적어주세요.")
+console.log(a2)
+
+let a3
+a3 = prompt("주문 수량을 적어주세요.")
+console.log(a3)
+
+console.log("[주문 접수]", "\n", 
+            "이름", "\n ", a1, "\n", "\n", 
+            "메뉴", "\n ", a2, "\n", "\n", 
+            "수량", "\n ", a3)
+
+
+console.log("[주문 접수]", 
+            "\n이름", "\n", a1, "\n",
+            "\n메뉴", "\n", a2, "\n",
+            "\n수량", "\n", a3)
+
+
+console.log("[주문 접수]")
+console.log("이름")
+console.log("", a1)
+console.log("")
+console.log("메뉴")
+console.log("", a2)
+console.log("")
+console.log("수량")
+console.log("", a3)
